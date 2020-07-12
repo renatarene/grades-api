d311cd18547cb0e534c050a83686150cf65804bb
Renata Ernestina <renatarene.silva573@gmail.com>
2020-07-12 12:50:12 -0300

    Adicionado Schema

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
+import gradeModel from './gradeModel';
 
 const db = {};
 db.mongoose = mongoose;
 db.url = process.env.MONGODB;
+db.gradeModel = gradeModel(mongoose);
 
 export { db };
